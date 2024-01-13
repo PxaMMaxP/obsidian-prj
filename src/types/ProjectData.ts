479e4e7c74ca46d6020282ae2226e55e48be212b
Max P <Mail@MPassarello.de>
2024-01-13 23:21:41 +0100

    # `*Data` - All four data classes have been adapted so that the correct type certainly exists in them, unless it is specified.

--- src/types/ProjectData.ts
+++ src/types/ProjectData.ts
@@ -16,7 +16,10 @@ export default class ProjectData implements IPrjData, IPrjTaskManagement {
     aliases: string[] | null | undefined;
 
     constructor(data: Partial<ProjectData>) {
-        if (!data) return;
+        if (!data) {
+            this.type = "Project";
+            return;
+        }
         this.aliases = data.aliases !== undefined ? data.aliases : undefined;
         this.title = data.title !== undefined ? data.title : undefined;
         this.description = data.description !== undefined ? data.description : undefined;
@@ -25,7 +28,7 @@ export default class ProjectData implements IPrjData, IPrjTaskManagement {
         this.energy = data.energy !== undefined ? data.energy : undefined;
         this.due = data.due !== undefined ? data.due : undefined;
         this.tags = data.tags !== undefined ? data.tags : undefined;
-        this.type = data.type !== undefined ? data.type : undefined;
+        this.type = data.type !== undefined ? data.type : "Project";
         this.subType = data.subType !== undefined ? data.subType : undefined;
         this.history = data.history !== undefined ? data.history : undefined;
     }
