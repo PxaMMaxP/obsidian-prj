14f5fb169383a62aa2e6ef8d72897ad4e9dc2035
Max P <Mail@MPassarello.de>
2024-02-04 14:14:44 +0100

    Refactor task management model and API
    Migrated from a static helper class to a dynamic PrjTaskManagementModel with new functionalities added. The API class now utilizes the dynamic model directly, removing the dependency on the static version. Deprecated methods were moved to the new dynamic model, and the corresponding deprecation warnings were added to guide future updates and maintenance. Additionally, functionality for syncing model status with file paths and title-to-filename consistency was introduced, along with a new method to calculate model urgency and sort models accordingly. This commit significantly enhances the codebase's modularity and sets the stage for easier enhancements and maintenance.
    
    Signed-off-by: Max P <Mail@MPassarello.de>

--- src/classes/API.ts
+++ src/classes/API.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import Global from 'src/classes/Global';
 import Helper from 'src/libs/Helper';
+import { PrjTaskManagementModel } from 'src/models/PrjTaskManagementModel';
 import { StaticDocumentModel } from 'src/models/StaticHelper/StaticDocumentModel';
 import { StaticNoteModel } from 'src/models/StaticHelper/StaticNoteModel';
-import { StaticPrjTaskManagementModel } from 'src/models/StaticHelper/StaticPrjTaskManagementModel';
 
 export default class API {
     public static get global(): Global {
         return Global.getInstance();
     }
     public static documentModel = StaticDocumentModel;
-    public static prjTaskManagementModel = StaticPrjTaskManagementModel;
+    public static prjTaskManagementModel = PrjTaskManagementModel;
     private static noteModel = StaticNoteModel;
     public static helper = Helper;
 }
