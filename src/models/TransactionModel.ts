635723b96b767ca69b3a4e1e826363d248ddc2aa
Max P <Mail@MPassarello.de>
2024-02-04 20:06:31 +0100

    Enhance file management in FileModel
    Refactored the FileModel to provide additional file-related operations, such as renaming, moving, and creating files. Adjusted the visibility of writeChangesPromise to protected in TransactionModel to accommodate the new methods in FileModel. Removed an unused import of FileManager from obsidian in favor of a specific import of FileManager and Filename from a local source, streamlining dependencies and improving code organization related to file operations.

--- src/models/TransactionModel.ts
+++ src/models/TransactionModel.ts
@@ -30,7 +30,7 @@ export class TransactionModel<T> {
      * A promise that resolves when the changes are written to the file.
      */
     private _writeChangesPromise: Promise<void> | undefined;
-    public get writeChangesPromise(): Promise<void> | undefined {
+    protected get writeChangesPromise(): Promise<void> | undefined {
         return this._writeChangesPromise;
     }
     private _transactionActive = false;
