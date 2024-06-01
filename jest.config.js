7b91e157683ffa683c16bcec5ae0a317c2d16b5d
Max P <Mail@MPassarello.de>
2024-06-01 13:49:03 +0200

    Update Jest configuration for module path resolution
    The Jest configuration has been updated to introduce module name mapping.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts']
+    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+    }
 };
