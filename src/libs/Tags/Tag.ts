ed91d058a5004f2f5891cccb3420c311cde7542c
Max P <Mail@MPassarello.de>
2024-06-30 15:40:31 +0200

    Implement ITag interface to enhance Tag class
    Refactored Tag class to implement the new ITag interface, increasing modularity and adhering to interface-driven design standards. Introduced new private methods and properties for tag management, including hash prefixing and cache checking. Added TagFactory and ITagFactory for factory creation pattern. Updated tests to cover new functionality and handle changes, ensuring comprehensive test coverage. Improved Tag class usability by providing additional methods such as getElements and getObsidianLink, enhancing tag manipulation capabilities.

--- src/libs/Tags/Tag.ts
+++ src/libs/Tags/Tag.ts
@@ -1,15 +1,77 @@
+import IMetadataCache from 'src/interfaces/IMetadataCache';
+import { ITag } from './interfaces/ITag';
+
 /**
  * Represents a tag.
  * @remarks The class extends the String class and provides additional methods to work with tags.
  */
-export default class Tag extends String {
+export default class Tag implements ITag {
+    private _tag: string;
+
+    /**
+     * The metadata cache.
+     */
+    private _metadataCache: IMetadataCache;
+
+    /**
+     * The tag with a hash symbol.
+     */
+    private _tagWithHash: string | undefined = undefined;
+
+    /**
+     * Gets the tag with a hash symbol.
+     * @returns The tag with a hash symbol.
+     * @remarks The tag is prefixed with a hash symbol if it doesn't already have one.
+     */
+    private get tagWithHash(): string {
+        if (!this._tagWithHash) {
+            this._tagWithHash = this._tag.startsWith('#')
+                ? this._tag
+                : `#${this._tag}`;
+        }
+
+        return this._tagWithHash;
+    }
+
+    /**
+     * Whether the tag exists in the cache.
+     */
+    private _exists: boolean | undefined = undefined;
+
+    /**
+     * Gets whether the tag exists in the cache.
+     * @returns Whether the tag exists in the cache.
+     * @remarks Lazy loading is used to check if the tag exists in the cache.
+     */
+    public get exists(): boolean {
+        if (this._exists === undefined) {
+            const existFile = this._metadataCache.cache.find((file) => {
+                const tags = file.metadata?.frontmatter?.tags;
+
+                if (typeof tags === 'string') {
+                    return tags === this.tagWithHash;
+                } else if (Array.isArray(tags)) {
+                    return tags.includes(this.tagWithHash);
+                }
+
+                return false;
+            });
+
+            this._exists = existFile ? true : false;
+        }
+
+        return this._exists;
+    }
+
     /**
      * Creates a new instance of the Tag class.
      * @param value The value of the tag.
+     * @param metadataCache The metadata cache. If not provided, the global metadata cache is used.
      */
-    constructor(value: string) {
-        super(value);
-        Object.setPrototypeOf(this, Tag.prototype);
+    constructor(value: string, metadataCache: IMetadataCache) {
+        this._tag = value;
+
+        this._metadataCache = metadataCache;
     }
 
     /**
@@ -17,7 +79,7 @@ export default class Tag extends String {
      * @returns The value of the tag as an uppercase string.
      */
     public toUpperCase(): string {
-        return this.toString().toUpperCase();
+        return this._tag.toUpperCase();
     }
 
     /**
@@ -25,7 +87,7 @@ export default class Tag extends String {
      * @returns The value of the tag as a lowercase string.
      */
     public toLowerCase(): string {
-        return this.toString().toLowerCase();
+        return this._tag.toLowerCase();
     }
 
     /**
@@ -34,7 +96,7 @@ export default class Tag extends String {
      * @returns The character at the specified index.
      */
     public charAt(index: number): string {
-        return this.toString().charAt(index);
+        return this._tag.charAt(index);
     }
 
     /**
@@ -43,6 +105,65 @@ export default class Tag extends String {
      * @returns Whether the tag includes the specified substring.
      */
     public includes(substring: string): boolean {
-        return this.toString().includes(substring);
+        return this._tag.includes(substring);
+    }
+
+    /**
+     * Return an array of elements from the tag.
+     * @returns An array of elements from the tag.
+     * @remarks The elements are separated by a forward slash.
+     */
+    public getElements(): string[] {
+        return this._tag.split('/');
+    }
+
+    /**
+     * Creates an Obsidian tag link element.
+     * @param tagLabel The label for the tag link; if not provided, the tag with a hash symbol is used.
+     * @returns An Obsidian tag link element as an anchor element.
+     */
+    public getObsidianLink(tagLabel?: string): HTMLAnchorElement {
+        const obsidianLink = document.createElement('a');
+        obsidianLink.href = this.tagWithHash;
+        obsidianLink.classList.add('tag');
+        obsidianLink.target = '_blank';
+        obsidianLink.rel = 'noopener';
+        obsidianLink.textContent = `${tagLabel ?? this.tagWithHash}`;
+
+        return obsidianLink;
+    }
+
+    /**
+     * Overrides the valueOf method to return the primitive string value.
+     * @returns The primitive string value of the tag.
+     */
+    public valueOf(): string {
+        return this._tag;
+    }
+
+    /**
+     * Overrides the toString method to return the string representation of the tag.
+     * @returns The string representation of the tag.
+     */
+    public toString(): string {
+        return this._tag;
+    }
+
+    /**
+     * Checks if another tag is equal to this one based on the tag string.
+     * @param other The other tag to compare.
+     * @returns Whether the tags are equal.
+     */
+    public equals(other: ITag): boolean {
+        return this._tag === other.toString();
+    }
+
+    /**
+     * Returns whether the tag starts with the specified search string.
+     * @param searchString The string to search for.
+     * @param position The position to start the search.
+     */
+    startsWith(searchString: string, position?: number): boolean {
+        return this._tag.startsWith(searchString, position);
     }
 }
--- src/libs/Tags/__tests__/Tag.test.ts
+++ src/libs/Tags/__tests__/Tag.test.ts
@@ -1,64 +1,149 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import IMetadataCache from 'src/interfaces/IMetadataCache';
 import Tag from '../Tag';
 
 describe('Tag', () => {
+    let metadataCacheMock: IMetadataCache;
+
+    beforeEach(() => {
+        metadataCacheMock = {
+            cache: [
+                {
+                    metadata: {
+                        frontmatter: {
+                            tags: ['#exampleTag'],
+                        },
+                    },
+                },
+            ],
+        } as unknown as IMetadataCache;
+    });
+
     // Constructor Tests
     test('should initialize with a string value', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         expect(tag.toString()).toBe('exampleTag');
     });
 
     // Method Tests
     test('should return the string value with toString', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         expect(tag.toString()).toBe('exampleTag');
     });
 
     test('should return the string value in uppercase with toUpperCase', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         expect(tag.toUpperCase()).toBe('EXAMPLETAG');
     });
 
     test('should return the string value in lowercase with toLowerCase', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         expect(tag.toLowerCase()).toBe('exampletag');
     });
 
     test('should return the character at a specific position with charAt', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         expect(tag.charAt(0)).toBe('e');
     });
 
     test('should check if the tag includes a specific substring with includes', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         expect(tag.includes('ample')).toBe(true);
         expect(tag.includes('notInTag')).toBe(false);
     });
 
     // Comparison Tests
-    test('should compare the Tag object with a string using ==', () => {
-        const tag = new Tag('exampleTag');
-        expect(tag == 'exampleTag').toBe(true);
+    test('should compare the Tag object with a string', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        expect(tag.toString() == 'exampleTag').toBe(true);
     });
 
-    test('should not strictly compare the Tag object with a string using ===', () => {
-        const tag = new Tag('exampleTag');
-        expect(tag === 'exampleTag').toBe(false);
+    test('should compare the Tag object with a object using ==', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        const equalTagString = new Tag('exampleTag', metadataCacheMock);
+        expect(tag.equals(equalTagString)).toBe(true);
     });
 
     test('should use tag object in template literals correctly', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         expect(`This is a tag: ${tag}`).toBe('This is a tag: exampleTag');
     });
 
-    // Additional Tests
     test('should treat Tag instances as strings in concatenation', () => {
-        const tag = new Tag('exampleTag');
+        const tag = new Tag('exampleTag', metadataCacheMock);
         const result = tag + ' is cool';
         expect(result).toBe('exampleTag is cool');
     });
 
-    test('should have the same prototype as String', () => {
-        const tag = new Tag('exampleTag');
-        expect(Object.getPrototypeOf(tag)).toBe(Tag.prototype);
+    // Test `exists` property
+    test('should return true if tag exists in metadata cache', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        expect(tag.exists).toBe(true);
+    });
+
+    test('should return false if tag does not exist in metadata cache', () => {
+        const metadataCacheMockEmpty: IMetadataCache = {
+            cache: [],
+        } as unknown as IMetadataCache;
+        const tag = new Tag('exampleTag', metadataCacheMockEmpty);
+        expect(tag.exists).toBe(false);
+    });
+
+    // Test `tagWithHash` property
+    test('should return tag with hash', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        const tagWithHash = tag['tagWithHash'];
+        expect(tagWithHash).toBe('#exampleTag');
+    });
+
+    // Test `getObsidianLink` method
+    test('should return correct obsidian link', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        const obsidianLink = tag.getObsidianLink();
+
+        expect(obsidianLink.href.replace('http://localhost/', '')).toBe(
+            '#exampleTag',
+        );
+        expect(obsidianLink.textContent).toBe('#exampleTag');
+    });
+
+    test('should return correct obsidian link with custom label', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        const obsidianLink = tag.getObsidianLink('Custom Label');
+
+        expect(obsidianLink.href.replace('http://localhost/', '')).toBe(
+            '#exampleTag',
+        );
+        expect(obsidianLink.textContent).toBe('Custom Label');
+    });
+
+    // Test `startsWith` method
+    test('should return true if tag starts with a specific substring', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        expect(tag.startsWith('example')).toBe(true);
+    });
+
+    test('should return false if tag does not start with a specific substring', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        expect(tag.startsWith('notInTag')).toBe(false);
+    });
+
+    test('should return true if tag starts with a specific substring at a specific position', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        expect(tag.startsWith('ample', 2)).toBe(true);
+    });
+
+    test('should return false if tag does not start with a specific substring at a specific position', () => {
+        const tag = new Tag('exampleTag', metadataCacheMock);
+        expect(tag.startsWith('ample', 3)).toBe(false);
+    });
+
+    // Test `getElements` method
+    test('should return an array of elements from the tag', () => {
+        const tag = new Tag('exampleTag/subtag', metadataCacheMock);
+        expect(tag.getElements()).toEqual(['exampleTag', 'subtag']);
     });
 });
