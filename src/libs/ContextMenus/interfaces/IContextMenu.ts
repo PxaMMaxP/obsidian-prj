0357a3529cfb85195529637fc77b7ddfd0d372bc
Max P <Mail@MPassarello.de>
2024-07-12 19:42:15 +0200

    Add `invoke` method to IContextMenu interface
    Added `invoke` method to the IContextMenu interface to handle the invocation of the command menu. This extends the capabilities of the context menu, allowing for asynchronous operations when the menu is triggered, which can facilitate more dynamic and responsive user interactions.

--- src/libs/ContextMenus/interfaces/IContextMenu.ts
+++ src/libs/ContextMenus/interfaces/IContextMenu.ts
@@ -5,5 +5,8 @@ import { ContextMenu } from '../ContextMenu';
  * Represents an interface for the {@link ContextMenu|context menu} class.
  */
 export interface IContextMenu extends IDeconstructor {
+    /**
+     * This method is called when the command menu is invoked.
+     */
     invoke(): Promise<void>;
 }
