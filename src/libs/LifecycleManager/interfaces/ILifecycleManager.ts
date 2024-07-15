8ca1ee57085960b62795837a6e7fd629ea5236ce
Max P <Mail@MPassarello.de>
2024-07-15 06:27:28 +0200

    Expand ILifecycleObject interface with lifecycle hooks
    Added detailed lifecycle hooks to the ILifecycleObject interface. Each stage (beforeInit, onInit, afterInit, beforeLoad, onLoad, afterLoad, beforeUnload, onUnload, afterUnload) now includes specific documentation and priority levels. These enhancements ensure a clear and structured lifecycle management process, aiding developers in aligning their components with application states.

--- src/libs/LifecycleManager/interfaces/ILifecycleManager.ts
+++ src/libs/LifecycleManager/interfaces/ILifecycleManager.ts
@@ -28,14 +28,53 @@ export interface ILifecycleManager {
  * @see {@link ImplementsStatic|ImplementsStatic for interface checking}.
  */
 export interface ILifecycleObject {
+    /**
+     * Will be called before the initialization state.
+     * Priority: **StartUp** (0)
+     * @remarks Use this stage for all setup tasks
+     * - that are base requirements for the application to run.
+     * - or that are not dependent on other services or modules.
+     */
     beforeInit?(): void | Promise<void>;
+    /**
+     * Will be called during the initialization state.
+     * Priority: **StartUp** (1)
+     */
     onInit?(): void | Promise<void>;
+    /**
+     * Will be called after the initialization state.
+     * Priority: **StartUp** (2)
+     */
     afterInit?(): void | Promise<void>;
+    /**
+     * Will be called before the loading state.
+     * Priority: **StartUp** (3)
+     */
     beforeLoad?(): void | Promise<void>;
+    /**
+     * Will be called during the loading state.
+     * Priority: **StartUp** (4)
+     */
     onLoad?(): void | Promise<void>;
+    /**
+     * Will be called after the loading state.
+     * Priority: **StartUp** (5)
+     */
     afterLoad?(): void | Promise<void>;
+    /**
+     * Will be called before the unloading state.
+     * Priority: **Shutdown** (0)
+     */
     beforeUnload?(): void | Promise<void>;
+    /**
+     * Will be called during the unloading state.
+     * Priority: **Shutdown** (1)
+     */
     onUnload?(): void | Promise<void>;
+    /**
+     * Will be called after the unloading state.
+     * Priority: **Shutdown** (2)
+     */
     afterUnload?(): void | Promise<void>;
 }
 
