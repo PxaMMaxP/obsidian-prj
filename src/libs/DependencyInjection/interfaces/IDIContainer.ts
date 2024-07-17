d087311ed531e9f554f55eb227e52ce629963a03
Max P <Mail@MPassarello.de>
2024-07-17 21:03:01 +0200

    Add 'necessary' parameter to resolve method in DI container
    Updated the `resolve` method in the Dependency Injection (DI) container interface to include an optional 'necessary' parameter. This parameter allows the method to throw an error if a dependency is not found, enhancing error handling and making dependency resolution more robust.

--- src/libs/DependencyInjection/interfaces/IDIContainer.ts
+++ src/libs/DependencyInjection/interfaces/IDIContainer.ts
@@ -20,6 +20,13 @@ export interface IDIContainer {
      * Resolve a dependency
      * @param identifier The identifier of the dependency
      * @param necessary If true, throws an error if the dependency is not found
+     * @returns The resolved dependency or undefined if the dependency is not found
+     */
+    resolve<T>(identifier: string, necessary?: boolean): T;
+    /**
+     * Resolve a dependency
+     * @param identifier The identifier of the dependency
+     * @param necessary If true, throws an error if the dependency is not found
      * @returns The resolved dependency
      * @throws Error if the dependency is not found
      */
