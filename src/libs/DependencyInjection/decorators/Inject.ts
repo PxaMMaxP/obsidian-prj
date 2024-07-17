259ac4b27e15f37b6683e9cac636423516dcf6da
Max P <Mail@MPassarello.de>
2024-07-18 01:18:20 +0200

    Refactor: Move `InitDelegate` to a separate file
    Relocated the `InitDelegate` type definition from `Inject.ts` to its own file in `types/InitDelegate.ts`. This enhances code organization and promotes clearer separation of concerns within the dependency injection library.

--- src/libs/DependencyInjection/decorators/Inject.ts
+++ src/libs/DependencyInjection/decorators/Inject.ts
@@ -1,14 +1,5 @@
 import { DIContainer } from '../DIContainer';
-
-/**
- * A function type representing an initializer that transforms an input of type `T`
- * into an output of type `U`.
- * @template T - The type of the input parameter.
- * @template U - The type of the output parameter.
- * @param x - The input parameter of type `T`.
- * @returns The transformed output of type `U`.
- */
-export type InitDelegate<T, U> = (x: T) => U;
+import { InitDelegate } from '../types/InitDelegate';
 
 /**
  * A decorator to inject a dependency from a DI (Dependency Injection) container.
