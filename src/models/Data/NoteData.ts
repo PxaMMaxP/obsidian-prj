1294a9d978e21cae5f4a3c41d32d32f1cb6a3c75
Max P <Mail@MPassarello.de>
2024-07-17 13:08:21 +0200

    Refactor NoteData for improved consistency
    Reorganized NoteData class to extend PrjData and implement IPrjNote, aligning with project data structures. Simplified dependency management by moving some dependency resolutions to PrjData. Removed redundant fields and methods, ensuring streamlined code and enhancing maintainability.

--- src/models/Data/NoteData.ts
+++ src/models/Data/NoteData.ts
@@ -1,93 +1,39 @@
 import { fieldConfig } from 'src/classes/decorators/FieldConfigDecorator';
+import { ImplementsStatic } from 'src/classes/decorators/ImplementsStatic';
 import { toStringField } from 'src/classes/decorators/ToStringFieldDecorator';
-import { IDIContainer } from 'src/libs/DependencyInjection/interfaces/IDIContainer';
-import { IFileType, IFileType_ } from 'src/libs/FileType/interfaces/IFileType';
-import { ITag } from 'src/libs/Tags/interfaces/ITag';
-import { ITags, ITags_ } from 'src/libs/Tags/interfaces/ITags';
-import { IPrjData } from 'src/models/Data/interfaces/IPrjData';
-import BaseData from './BaseData';
+import { YamlKeyMap } from 'src/types/YamlKeyMap';
+import { IPrjData_ } from './interfaces/IPrjData';
+import { IPrjNote } from './interfaces/IPrjNote';
+import { PrjData } from './PrjData';
 
 /**
  * Represents a note.
  */
-export default class NoteData extends BaseData<NoteData> implements IPrjData {
-    private _iTags: ITags_;
-    private _iFileType: IFileType_;
-
+@ImplementsStatic<IPrjData_<unknown>>()
+export default class NoteData extends PrjData<NoteData> implements IPrjNote {
     /**
-     * The type of the note.
-     */
-    private _type: IFileType | null | undefined;
-
-    /**
-     * Sets the type of the note.
-     */
-    @fieldConfig('Note')
-    set type(value: unknown) {
-        this._type = new this._iFileType(value);
-    }
-
-    /**
-     * Gets the type of the note.
-     */
-    get type(): IFileType | null | undefined {
-        return this._type as IFileType | null | undefined;
-    }
-
-    @toStringField
-    @fieldConfig()
-    title: string | null | undefined;
-
-    @toStringField
-    @fieldConfig()
-    description: string | null | undefined;
-
-    @toStringField
-    @fieldConfig()
-    date: string | null | undefined;
-
-    /**
-     * The tags of the note.
-     * @remarks This value is included in the `toString` output.
-     */
-    private _tags: ITags | null | undefined;
-
-    /**
-     * Sets the tags of the note.
-     */
-    @fieldConfig()
-    @toStringField
-    set tags(value: ITags | ITag | string | string[] | null | undefined) {
-        if (this._iTags.isInstanceOf(value)) {
-            this._tags = value;
-        } else {
-            this._tags = new this._iTags(value);
-        }
-    }
-
-    /**
-     * Gets the tags of the note.
-     */
-    get tags(): ITags | null | undefined {
-        return this._tags;
-    }
-
-    /**
-     * Initializes a new instance of the NoteData class.
-     * @param data - The data to use for the model.
-     * - If no data is provided, the default values e.g. `undefined` are used.
-     * - If only partial data is provided, the missing values are set to `undefined`.
-     * @param dependencies  The optional dependencies to use for the model. {@link BaseData}
-     */
-    constructor(data?: Partial<NoteData>, dependencies?: IDIContainer) {
-        super(data, dependencies);
-    }
-
-    /**
-     * Initializes the dependencies of the class.
+     * @inheritdoc
      */
     protected initializeDependencies(): void {
-        this._iTags = this._dependencies.resolve<ITags_>('ITags_');
-        this._iFileType = this._dependencies.resolve<IFileType_>('IFileType_');
+        super.initializeDependencies();
+    }
+
+    private _date: string | null | undefined;
+
+    static yamlKeyMap: YamlKeyMap | undefined = {};
+
+    /**
+     * @inheritdoc
+     */
+    @toStringField
+    @fieldConfig()
+    get date(): string | null | undefined {
+        return this._date;
+    }
+    /**
+     * @inheritdoc
+     */
+    set date(value: string | null | undefined) {
+        this._date = value;
     }
 }
