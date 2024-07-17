3c358909d858656263de45dd1e0fe61eebdf3632
Max P <Mail@MPassarello.de>
2024-07-17 22:41:25 +0200

    Add deprecation support to register method in DI container
    Added overloaded register methods in the DI container interface to support marking dependencies as deprecated. This allows logging a warning when a deprecated dependency is resolved, improving the management and tracking of deprecated components.

--- src/libs/DependencyInjection/interfaces/IDIContainer.ts
+++ src/libs/DependencyInjection/interfaces/IDIContainer.ts
@@ -17,6 +17,28 @@ export interface IDIContainer {
     register<T>(identifier: string, dependency: T): void;
 
     /**
+     * Register a dependency
+     * @param identifier The identifier of the dependency
+     * @param dependency The dependency to register
+     * @param deprecated If true, the dependency is deprecated => a warning
+     * is logged when the dependency is resolved
+     */
+    register<T>(identifier: string, dependency: T, deprecated?: boolean): void;
+    /**
+     * Register a dependency
+     * @param identifier The identifier of the dependency
+     * @param dependency The dependency to register
+     * @param deprecated A warning is logged when the dependency is resolved
+     */
+    register<T>(identifier: string, dependency: T, deprecated?: true): void;
+    /**
+     * Register a dependency
+     * @param identifier The identifier of the dependency
+     * @param dependency The dependency to register
+     */
+    register<T>(identifier: string, dependency: T): void;
+
+    /**
      * Resolve a dependency
      * @param identifier The identifier of the dependency
      * @param necessary If true, throws an error if the dependency is not found
