bd8bbd5ff5cd1f0775c99ab192dd63a6982610e7
Max P <Mail@MPassarello.de>
2024-07-19 10:47:17 +0200

    Refactor DI decorator to enhance resilience and clarity
    Refactored the `Inject` decorator to separate dependency evaluation and initialization logic into distinct functions (`evaluate` and `tryInit`). This enhances error resilience and improves code readability. Also corrected property existence check logic and expanded documentation for better clarity.

--- src/libs/DependencyInjection/decorators/Inject.ts
+++ src/libs/DependencyInjection/decorators/Inject.ts
@@ -2,12 +2,50 @@ import { DIContainer } from '../DIContainer';
 import { InitDelegate } from '../types/InitDelegate';
 
 /**
+ * Evaluate a dependency from a DI (Dependency Injection) container.
+ * @param identifier The identifier used to resolve the dependency from the DI container.
+ * @param necessary Indicates if the dependency is necessary.
+ * @returns The resolved dependency or `undefined` if the dependency is not found and not necessary.
+ */
+const evaluate = (identifier: string, necessary: boolean): unknown => {
+    const diContainer = DIContainer.getInstance();
+
+    return diContainer.resolve<unknown>(identifier, necessary);
+};
+
+/**
+ * Try to initialize a dependency with an optional initializer function.
+ * @param init A function to transform the dependency before injection.
+ * @param dependency The dependency to be transformed.
+ * @param necessary Indicates if the dependency is necessary.
+ * @returns The transformed dependency or `undefined` if the transformation fails and the dependency is not necessary.
+ */
+const tryInit = <DependencyType, FieldType>(
+    init: InitDelegate<DependencyType, FieldType>,
+    dependency: DependencyType,
+    necessary: boolean,
+): FieldType => {
+    try {
+        return init(dependency);
+    } catch (error) {
+        if (necessary) {
+            throw new Error(
+                `Failed to initialize the dependency: ${error.message}`,
+            );
+        } else {
+            return undefined as unknown as FieldType;
+        }
+    }
+};
+
+/**
  * A decorator to inject a dependency from a DI (Dependency Injection) container.
  * The dependency is lazily evaluated when the property is accessed for the first time.
  * This can help avoid issues like circular dependencies and not-found dependencies.
  * @template ClassType The type of the property to be injected.
  * @param identifier The identifier used to resolve the dependency from the DI container.
  * @param init An optional initializer function to transform the dependency before injection.
+ * It get the raw dependency as input and returns the transformed dependency.
  * @param necessary Indicates if the dependency is necessary.
  * - If `true`, an error will be thrown if the dependency cannot be resolved.
  * - If `false`, `undefined` will be returned if the dependency cannot be resolved.
@@ -28,47 +66,39 @@ import { InitDelegate } from '../types/InitDelegate';
  * }
  * ```
  */
-export function Inject<T, U>(
+export function Inject<FieldType, DependencyType = FieldType>(
     identifier: string,
-    init?: InitDelegate<T, U>,
+    init?: InitDelegate<DependencyType, FieldType>,
     necessary = true,
 ) {
-    return function (target: unknown, propertyKey: string | symbol): void {
+    return function <ClassType>(
+        target: ClassType,
+        propertyKey: string | symbol,
+    ): void {
         // Unique symbol to store the private property
         const privatePropertyKey: unique symbol = Symbol();
-        // Get the DI container instance
-        const diContainer = DIContainer.getInstance();
-
-        // Function to evaluate the dependency lazily
-        // to avoid circular dependencies, not found dependencies, etc.
-        const evaluate = () => {
-            return diContainer.resolve<unknown>(identifier, necessary);
-        };
 
         // Define the property
         Object.defineProperty(target, propertyKey, {
             get() {
                 // If the property is not defined, evaluate the dependency
-                if (!this.hasOwnProperty(privatePropertyKey)) {
+                if (this.hasOwnProperty(privatePropertyKey) == null) {
                     if (init) {
-                        try {
-                            this[privatePropertyKey] = init(evaluate() as T);
-                        } catch (error) {
-                            if (necessary) {
-                                throw error;
-                            }
-                        }
+                        tryInit(
+                            init,
+                            evaluate(identifier, necessary),
+                            necessary,
+                        );
                     } else {
-                        this[privatePropertyKey] = evaluate();
+                        this[privatePropertyKey] = evaluate(
+                            identifier,
+                            necessary,
+                        );
                     }
                 }
 
                 return this[privatePropertyKey];
             },
-            // Not necessary to set the property
-            // set(value: PropertieType) {
-            //     this[privatePropertyKey] = value;
-            // },
             enumerable: true,
             configurable: false,
         });
