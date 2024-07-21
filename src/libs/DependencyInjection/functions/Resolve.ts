dc796ffaef7a7229956d857885e41f1b4407fb1f
Max P <Mail@MPassarello.de>
2024-07-21 13:45:26 +0200

    Refactor DIContainer singleton and add Resolve function
    Standardized the singleton pattern in DIContainer by modifying `getInstance` to return `IDIContainer`. Enhanced `register` and `resolve` methods for better type support. Introduced a new `Resolve` function for resolving dependencies using configurable necessity checks. Adjusted the IDIContainer interface to reflect these changes, ensuring better consistency and readability in dependency injection management.

---
+++ src/libs/DependencyInjection/functions/Resolve.ts
@@ -0,0 +1,29 @@
+import { DIContainer } from '../DIContainer';
+
+/**
+ * Resolve a dependency
+ * @param identifier The identifier of the dependency
+ * @returns The resolved dependency
+ * @throws Error if the dependency is not found
+ */
+export function Resolve<T>(identifier: string): T;
+/**
+ * Resolve a dependency
+ * @param identifier The identifier of the dependency
+ * @param necessary If true, throws an error if the dependency is not found
+ * @returns The resolved dependency or undefined if the dependency is not found
+ */
+export function Resolve<T>(identifier: string, necessary: false): T | undefined;
+
+/**
+ * Resolve a dependency
+ * @param identifier The identifier of the dependency
+ * @param necessary If true, throws an error if the dependency is not found
+ * @returns The resolved dependency or undefined if the dependency is not found
+ */
+export function Resolve<T>(
+    identifier: string,
+    necessary?: boolean,
+): T | undefined {
+    return DIContainer.getInstance().resolve<T>(identifier, necessary);
+}
