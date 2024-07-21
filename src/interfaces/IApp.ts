0ed9c4998a09c33d8d456213f86e6fe7a17749a3
Max P <Mail@MPassarello.de>
2024-07-21 13:46:27 +0200

    Add DI interfaces for App and Prj classes
    Introduced IApp and IPrj interfaces to improve type safety and clarity in dependency injection. Adjusted DIContainer registrations to include these new interfaces alongside their respective classes.

---
+++ src/interfaces/IApp.ts
@@ -0,0 +1,8 @@
+import { App } from 'obsidian';
+
+/**
+ * Represents the interface for the App class.
+ * @see {@link App}
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IApp extends App {}
