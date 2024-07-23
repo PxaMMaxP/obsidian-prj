e2097564d0414663696a0f4a2aabba3242b00459
Max P <Mail@MPassarello.de>
2024-07-23 20:01:40 +0200

    Refactor modal initialization logic
    Enhanced `CustomModal` class by refactoring modal element creation to use getters and updating the constructor to accept `isDraggable` and `willDimBackground` parameters. Improved clarity and maintainability by centralizing element initialization and event binding in dedicated methods. This refactor better handles optional draggable functionality and conditional background dimming.

--- src/libs/Modals/CustomModal/CustomModal.ts
+++ src/libs/Modals/CustomModal/CustomModal.ts
@@ -3,7 +3,10 @@ import type { IApp } from 'src/interfaces/IApp';
 import { Inject } from 'src/libs/DependencyInjection/decorators/Inject';
 import type { ILifecycleManager_ } from 'src/libs/LifecycleManager/interfaces/ILifecycleManager';
 import { ICustomModal } from './interfaces/ICustomModal';
-import type { IDraggableElement_ } from './interfaces/IDraggableElement';
+import type {
+    IDraggableElement,
+    IDraggableElement_,
+} from './interfaces/IDraggableElement';
 
 /**
  * Represents a custom modal, which can be dragged around
@@ -18,16 +21,134 @@ export abstract class CustomModal implements ICustomModal {
     private readonly _IDraggableElement_!: IDraggableElement_;
 
     private readonly _beforeUnload: () => void = this.close.bind(this);
-    protected _IComponent = new Component();
+    protected _IComponent: Component;
+    private _draggableElement?: IDraggableElement;
+
+    private readonly _isDraggable: boolean;
+    private readonly _willDimBackground: boolean;
 
     private readonly _body: HTMLElement = document.body;
-    private _container: HTMLElement;
-    private _title: HTMLElement;
-    private _modal: HTMLElement;
+    private __container?: HTMLElement;
+    private __bg?: HTMLElement;
+    private __closeButton?: HTMLElement;
+    private __title?: HTMLElement;
+    private __modal?: HTMLElement;
+    private __content?: HTMLElement;
+
+    /**
+     * The container of the modal.
+     */
+    private get _container(): HTMLElement {
+        if (this.__container == null) {
+            this.__container = document.createElement('div');
+            this.__container.classList.add('modal-container', 'mod-dim');
+
+            if (!this._willDimBackground)
+                this.__container.style.pointerEvents = 'none';
+        }
+
+        return this.__container;
+    }
+
+    /**
+     * The background of the modal, if it will be dimmed
+     * else an empty fragment.
+     */
+    private get _bg(): DocumentFragment {
+        if (this._willDimBackground && this.__bg == null) {
+            this.__bg = document.createElement('div');
+            this.__bg.classList.add('modal-bg');
+            this.__bg.style.opacity = '0.85';
+
+            // Add event listener to close the modal
+            // if the background is clicked
+            this._IComponent.registerDomEvent(
+                this.__bg,
+                'click',
+                this.close.bind(this),
+            );
+        }
+
+        const fragment = new DocumentFragment();
+
+        if (this._willDimBackground && this.__bg != null)
+            fragment.appendChild(this.__bg);
+
+        return fragment;
+    }
+
+    /**
+     * The close button of the modal.
+     */
+    private get _closeButton(): HTMLElement {
+        if (this.__closeButton == null) {
+            this.__closeButton = document.createElement('div');
+            this.__closeButton.classList.add('modal-close-button');
+        }
+
+        // Add event listener to close the modal
+        this._IComponent.registerDomEvent(
+            this.__closeButton,
+            'click',
+            this.close.bind(this),
+        );
+
+        return this.__closeButton;
+    }
+
+    /**
+     * The title of the modal.
+     */
+    private get _title(): HTMLElement {
+        if (this.__title == null) {
+            this.__title = document.createElement('div');
+            this.__title.classList.add('modal-title');
+            this.__title.innerText = '\u00A0';
+        }
+
+        return this.__title;
+    }
+
+    /**
+     * The modal element.
+     */
+    private get _modal(): HTMLElement {
+        if (this.__modal == null) {
+            this.__modal = document.createElement('div');
+            this.__modal.classList.add('modal');
+            this.__modal.style.pointerEvents = 'auto';
+        }
+
+        return this.__modal;
+    }
+
     /**
      * The content of the modal.
      */
-    protected _content: HTMLElement;
+    protected get _content(): HTMLElement {
+        if (this.__content == null) {
+            this.__content = document.createElement('div');
+            this.__content.classList.add('modal-content');
+        }
+
+        return this.__content;
+    }
+
+    /**
+     * Creates a new Modal.
+     * @param isDraggable Whether the modal should be draggable.
+     * @param willDimBackground Whether the background should be dimmed.
+     * @param component The component that the modal belongs to.
+     */
+    constructor(
+        isDraggable: boolean,
+        willDimBackground: boolean,
+        component: Component = new Component(),
+    ) {
+        this._isDraggable = isDraggable;
+        this._willDimBackground = willDimBackground;
+        this._IComponent = component;
+    }
 
     /**
      * Opens the modal.
@@ -43,6 +164,10 @@ export abstract class CustomModal implements ICustomModal {
 
         this._IComponent.load();
         this.buildModal();
+
+        if (this._isDraggable && this._draggableElement != null) {
+            this._draggableElement.enableDragging();
+        }
         this.onOpen();
     }
 
@@ -67,44 +192,24 @@ export abstract class CustomModal implements ICustomModal {
      */
     private buildModal(): void {
         const fragment = new DocumentFragment();
-        this._container = document.createElement('div');
-        this._container.classList.add('modal-container', 'mod-dim');
-        this._container.style.pointerEvents = 'none';
+
+        this._container.appendChild(this._bg);
         fragment.appendChild(this._container);
 
-        this._modal = document.createElement('div');
-        this._modal.classList.add('modal');
-        this._modal.style.pointerEvents = 'auto';
-
-        const closeButton = document.createElement('div');
-        closeButton.classList.add('modal-close-button');
-
-        // Add event listener to close the modal
-        this._IComponent.registerDomEvent(
-            closeButton,
-            'click',
-            this.close.bind(this),
-        );
-        this._modal.appendChild(closeButton);
-
-        this._title = document.createElement('div');
-        this._title.classList.add('modal-title');
-        this._title.innerText = '\u00A0';
+        this._modal.appendChild(this._closeButton);
         this._modal.appendChild(this._title);
-
-        this._content = document.createElement('div');
-        this._content.classList.add('modal-content');
         this._modal.appendChild(this._content);
-
         this._container.appendChild(this._modal);
 
         this._body.appendChild(fragment);
 
-        new this._IDraggableElement_(
-            this._modal,
-            this._title,
-            this._IComponent,
-        ).enableDragging();
+        if (this._isDraggable) {
+            this._draggableElement = new this._IDraggableElement_(
+                this._modal,
+                this._title,
+                this._IComponent,
+            );
+        }
     }
 
     /**
