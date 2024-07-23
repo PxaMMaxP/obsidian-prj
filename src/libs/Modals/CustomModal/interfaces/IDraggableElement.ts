e12f577fc7f273df1d31fa677a02465c14e5a787
Max P <Mail@MPassarello.de>
2024-07-23 18:58:18 +0200

    Refactor and add draggable element interface
    Renamed `ICustomModal.ts` for better clarity and organization. Introduced `IDraggableElement` to define draggable behavior, which includes methods for initialization and enabling dragging. This enhances modularity and reusability of modal components.

---
+++ src/libs/Modals/CustomModal/interfaces/IDraggableElement.ts
@@ -0,0 +1,30 @@
+import { Component } from 'obsidian';
+
+/**
+ * Static interface for the draggable element.
+ * @see {@link IDraggableElement}
+ */
+export interface IDraggableElement_ {
+    /**
+     * Creates a new instance of {@link IDraggableElement}.
+     * @param draggableElement The element that should be draggable.
+     * @param dragHandle The element that should be used as the drag handle.
+     * @param component The component that the draggable element belongs to.
+     */
+    new (
+        draggableElement: HTMLElement,
+        dragHandle: HTMLElement,
+        component: Component,
+    ): IDraggableElement;
+}
+
+/**
+ * Interface for the draggable element.
+ * @see {@link IDraggableElement_}
+ */
+export interface IDraggableElement {
+    /**
+     * Enables dragging for the draggable element.
+     */
+    enableDragging(): void;
+}
