050cb11ef02c8e029e52228fe4c055e72c6a388c
Max P <Mail@MPassarello.de>
2024-07-23 21:40:54 +0200

    Enhance Custom Modal interface
    Refactored `ICustomModal` to include setter methods for callback functions, title, content, and modal properties. Updated the constructor to remove parameters related to modal behavior (draggable, dim background). Added detailed callback function interfaces and error handling annotations to improve robustness and enhance usability.

--- src/libs/Modals/CustomModal/interfaces/ICustomModal.ts
+++ src/libs/Modals/CustomModal/interfaces/ICustomModal.ts
@@ -1,4 +1,5 @@
 import { Component } from 'obsidian';
+import { CallbackError, MissingCallbackError } from './Exceptions';
 
 /**
  * Static interface for the custom modal.
@@ -7,16 +8,10 @@ import { Component } from 'obsidian';
 export interface ICustomModal_ {
     /**
      * Creates a new Modal.
-     * @param isDraggable Whether the modal should be draggable.
-     * @param willDimBackground Whether the background should be dimmed.
      * @param component The component that the modal belongs to.
      * @see {@link ICustomModal}
      */
-    new (
-        isDraggable: boolean,
-        willDimBackground: boolean,
-        component?: Component,
-    ): ICustomModal;
+    new (component?: Component): ICustomModal;
 }
 
 /**
@@ -25,24 +20,98 @@ export interface ICustomModal_ {
  */
 export interface ICustomModal {
     /**
+     * The content of the modal.
+     */
+    get content(): HTMLElement;
+
+    /**
      * Opens the modal.
+     * @throws {MissingCallbackError} If the `onOpen` callback is missing.
+     * @throws {CallbackError} If an error occurs in the `shouldOpen` callback.
+     * @throws {CallbackError} If an error occurs in the `onOpen` callback.
      */
     open(): void;
 
     /**
      * Closes the modal.
+     * @throws {CallbackError} If an error occurs in the `onClose` callback.
      */
     close(): void;
 
     /**
-     * Sets the title of the modal.
-     * @param title The title to set.
+     * Sets the before open callback.
+     * You can use this to check if the modal can be opened.
+     * If the callback returns false, the modal won't be opened.
+     * @param shouldOpen The callback to set.
+     * @remarks **Optional**
+     * @returns This instance.
      */
-    setTitle(title: string): void;
+    setShouldOpen(shouldOpen: IShouldOpenCallback): this;
 
     /**
-     * Sets the content of the modal.
-     * @param content The content to replace the current content with.
+     * Sets the on open callback.
+     * Will be called when the modal is opened.
+     * @param onOpen The callback to set.
+     * @returns This instance.
      */
-    setContent(content: DocumentFragment): void;
+    setOnOpen(onOpen: IOpenCallback): this;
+
+    /**
+     * Sets the on close callback.
+     * Will be called when the modal is closed.
+     * @param onClose The callback to set.
+     * @remarks **Optional**
+     * @returns This instance.
+     */
+    setOnClose(onClose: ICloseCallback): this;
+
+    /**
+     * Sets the title of the modal.
+     * @param title The title to set.
+     * @returns This instance.
+     */
+    setTitle(title: string): this;
+
+    /**
+     * Appends content to the modal content.
+     * @param content The content to append.
+     * @returns This instance.
+     */
+    setContent(content: DocumentFragment): this;
+
+    /**
+     * Sets the draggable state of the modal.
+     * @param isDraggable Whether the modal should be draggable.
+     * @returns This instance.
+     */
+    setDraggableEnabled(isDraggable: boolean): this;
+
+    /**
+     * Sets whether the background should be dimmed.
+     * @param willDimBackground Whether the background should be dimmed.
+     * @returns This instance.
+     */
+    setBackgroundDimmed(willDimBackground: boolean): this;
+}
+
+/**
+ * Interface for the callback to determine if the modal should open.
+ * @returns {boolean} True if the modal can be opened, otherwise false.
+ */
+export interface IShouldOpenCallback {
+    (): boolean;
+}
+
+/**
+ * Interface for the callback to be called when the modal is opened.
+ */
+export interface IOpenCallback {
+    (): void;
+}
+
+/**
+ * Interface for the callback to be called when the modal is closed.
+ */
+export interface ICloseCallback {
+    (): void;
 }
