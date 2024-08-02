f4d4835b699ffec02e1eecaae05c14afc4371fd4
Max P <Mail@MPassarello.de>
2024-08-02 13:13:27 +0200

    Add interfaces for setting and setting field items
    Introduced new TypeScript interfaces defining the structure and behavior of setting items and setting fields within the application. These interfaces help standardize the creation and manipulation of setting fields, ensuring consistent integration and extendibility in custom modals and components.

---
+++ src/libs/SettingFields/interfaces/ISettingField.ts
@@ -0,0 +1,26 @@
+import { Component } from 'obsidian';
+import { IInternalSettingItem } from './ISetting';
+
+export type SettingFieldConfigurator<
+    T extends new (...args: unknown[]) => unknown,
+> = (instance: InstanceType<T>) => void;
+
+export interface ISettingField_<
+    Type extends new (...args: unknown[]) => unknown,
+> {
+    /**
+     * @param settingField The setting item that the setting field belongs to.
+     * @param configurator A function that configures the setting field.
+     */
+    new (
+        parent: IInternalSettingItem,
+        configure: SettingFieldConfigurator<Type>,
+    ): ISettingField;
+}
+
+export interface ISettingField extends Component {
+    /**
+     * Apply the {@link SettingFieldConfigurator} to the setting item.
+     */
+    onload(): void;
+}
