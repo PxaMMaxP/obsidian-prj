e3780c74162e5195c64df79fbdde9cd32eca5155
Max P <Mail@MPassarello.de>
2024-08-09 19:31:44 +0200

    Add Button component with fluent API
    Introduced a new Button component, extending DIComponent. This component allows for configuration through a fluent API, supporting methods to set text, CTA status, click callbacks, and disabled state. Updated interfaces to include IButtonInternal and IButtonFluentAPI for better type safety and clear structure.

---
+++ src/libs/SettingFields/Fields/Button.ts
@@ -0,0 +1,144 @@
+import type { ILogger_, ILogger } from 'src/interfaces/ILogger';
+import { Inject } from 'src/libs/DependencyInjection/decorators/Inject';
+import { DIComponent } from 'src/libs/DIComponent/DIComponent';
+import type { IFlow_, IFlowApi } from 'src/libs/HTMLFlow/interfaces/IFlow';
+import { IFlowConfig } from 'src/libs/HTMLFlow/types/IFlowDelegates';
+import {
+    IButtonFluentAPI,
+    IButtonInternal,
+    OnClickCallback,
+} from './interfaces/IButton';
+import { SettingFieldConfigurator } from '../interfaces/ISettingField';
+import { IInternalSettingItem } from '../interfaces/SettingItem';
+
+/**
+ * A button field.
+ */
+export class Button extends DIComponent implements IButtonInternal {
+    @Inject('ILogger_', (x: ILogger_) => x.getLogger(''), false)
+    protected _logger?: ILogger;
+    @Inject('IFlow_')
+    protected readonly _IFlow_!: IFlow_;
+
+    private readonly _flowConfig: IFlowConfig<keyof HTMLElementTagNameMap> = (
+        cfg: IFlowApi<keyof HTMLElementTagNameMap>,
+    ) => {
+        cfg.appendChildEl('button', (cfg) => {
+            cfg.getEl((el) => (this._buttonEl = el))
+                .addClass(this._settings.cta ? 'mod-cta' : '')
+                .setTextContent(this._settings.text)
+                .setAttribute(
+                    'disabled',
+                    this._settings.isDisabled ? 'true' : undefined,
+                )
+                .addEventListener('click', (el, ev, flow) => {
+                    this._settings.onClick?.(ev);
+                });
+        });
+    };
+
+    private _buttonEl: HTMLButtonElement;
+    private readonly _settings: IButtonSettings = {
+        text: '',
+        cta: false,
+        onClick: undefined,
+        isDisabled: false,
+    };
+    /**
+     * @inheritdoc
+     */
+    public readonly parentSettingItem: IInternalSettingItem;
+    private readonly _configurator?: SettingFieldConfigurator<IButtonFluentAPI>;
+
+    /**
+     * @inheritdoc
+     */
+    get buttenEl(): HTMLButtonElement {
+        return this._buttonEl;
+    }
+
+    /**
+     * Creates a new instance of the button field.
+     * @param parentSettingItem The parent setting item.
+     * @param configurator The configurator of the button field.
+     */
+    constructor(
+        parentSettingItem: IInternalSettingItem,
+        configurator?: SettingFieldConfigurator<IButtonFluentAPI>,
+    ) {
+        super();
+
+        this.parentSettingItem = parentSettingItem;
+
+        this._configurator = configurator;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public override onload(): void {
+        this._configurator?.(this);
+
+        const flow = new this._IFlow_(
+            this.parentSettingItem.inputEl,
+            this._flowConfig,
+        );
+
+        this.addChild(flow);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    setCta(isCtaEnabled: boolean): IButtonFluentAPI {
+        this._settings.cta = isCtaEnabled;
+
+        return this;
+    }
+    /**
+     * @inheritdoc
+     */
+    onClick(onClick: OnClickCallback): IButtonFluentAPI {
+        this._settings.onClick = onClick;
+
+        return this;
+    }
+    /**
+     * @inheritdoc
+     */
+    setButtonText(text: string): IButtonFluentAPI {
+        this._settings.text = text;
+
+        return this;
+    }
+    /**
+     * @inheritdoc
+     */
+    setDisabled(shouldDisabled: boolean): IButtonFluentAPI {
+        this._settings.isDisabled = shouldDisabled;
+
+        return this;
+    }
+}
+
+/**
+ * Represents the settings of a button field.
+ */
+interface IButtonSettings {
+    /**
+     * The text of the button.
+     */
+    text: string;
+    /**
+     * Whether **Call to Action** is enabled.
+     */
+    cta: boolean;
+    /**
+     * The callback that is called when the button is clicked.
+     */
+    onClick?: OnClickCallback;
+    /**
+     * Whether the button is disabled.
+     */
+    isDisabled: boolean;
+}
--- src/libs/SettingFields/Fields/interfaces/IButton.ts
+++ src/libs/SettingFields/Fields/interfaces/IButton.ts
@@ -1,6 +1,64 @@
-import { Component } from 'obsidian';
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { IDIComponent } from 'src/libs/DIComponent/interfaces/IDIComponent';
 import { ISettingField } from '../../interfaces/ISettingField';
+import { IInternalSettingItem } from '../../interfaces/SettingItem';
 
-export interface IButton extends Component, ISettingField {
+/**
+ * The internal button field.
+ */
+export interface IButtonInternal extends IButtonFluentAPI {
+    /**
+     * The button element.
+     */
+    get buttenEl(): HTMLButtonElement;
+
+    /**
+     * The parent setting item.
+     */
+    get parentSettingItem(): IInternalSettingItem;
+}
+
+/**
+ * Represents a button field.
+ */
+export interface IButton extends IDIComponent, ISettingField {
     // --
 }
+
+/**
+ * The fluent API for the button field.
+ */
+export interface IButtonFluentAPI extends IButton {
+    /**
+     * Enables or disables **Call to Action**.
+     * @param isCtaEnabled Whether **Call to Action** is enabled.
+     * @returns The button fluent API.
+     */
+    setCta(isCtaEnabled: boolean): IButtonFluentAPI;
+
+    /**
+     * Sets the callback that is called when the button is clicked.
+     * @param onClick The callback that is called when the button is clicked.
+     * @returns The button fluent API.
+     */
+    onClick(onClick: OnClickCallback): IButtonFluentAPI;
+
+    /**
+     * Sets the text of the button.
+     * @param text The text of the button.
+     * @returns The button fluent API.
+     */
+    setButtonText(text: string): IButtonFluentAPI;
+
+    /**
+     * Disables or enables the button.
+     * @param shouldDisabled Whether the button should be disabled.
+     * @returns The button fluent API.
+     */
+    setDisabled(shouldDisabled: boolean): IButtonFluentAPI;
+}
+
+/**
+ * A callback that is called when the button is clicked.
+ */
+export type OnClickCallback = (evt: MouseEvent) => unknown;
