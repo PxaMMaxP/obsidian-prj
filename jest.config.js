c4b8ebf461b4946c42f168abb6b38b6e30e8e1a6
Max P <Mail@MPassarello.de>
2024-08-14 23:03:50 +0200

    Add ts-injex and update ESLint config and Jest setup
    Added ts-injex to dependencies.
    Added the @stylistic ESLint plugin and updated related configurations to enhance code style checks. Removed message customizations in ESLint regex for private and protected class properties. Disabled `no-unused-expressions` rule. In Jest configuration, mapped ts-injex to its source folder and ensured it's not ignored during transformations. Updated dependency versions to latest for better compatibility and performance.

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,12 @@ module.exports = {
     testPathIgnorePatterns: ['\\.spec\\.ts$', '\\.performance\\.test\\.ts$'],
     moduleDirectories: ['node_modules', 'src'],
     moduleNameMapper: {
-        '^src/(.*)$': '<rootDir>/src/$1',
+        '^src/(.*)$': '<rootDir>/src/$1', // Map src to the source folder
+        '^ts-injex$': '<rootDir>/node_modules/ts-injex/src', // Map ts-injex to the source folder
     },
+    transformIgnorePatterns: [
+        'node_modules/(?!ts-injex)' // **Dont** ignore ts-injex on preset `ts-jest`
+    ],
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov'],
