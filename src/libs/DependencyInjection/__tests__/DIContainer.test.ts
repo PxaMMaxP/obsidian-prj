09b5d4919763dcf95e8317135e5890b8bfab286f
Max P <Mail@MPassarello.de>
2024-08-14 23:24:11 +0200

    Remove custom DI container and related code
    Eliminated the custom dependency injection container and its associated interfaces, decorators, functions, and tests. Transitioned to using the `ts-injex` package for better maintainability and reducing redundant code. This makes the dependency injection process more standardized and easier to manage.

--- src/libs/DependencyInjection/__tests__/DIContainer.test.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable deprecation/deprecation */
-import { test_IDIContainer } from './IDIContainer.spec';
-import { DIContainer } from '../DIContainer';
-
-test_IDIContainer(DIContainer);
