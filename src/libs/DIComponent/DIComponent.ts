443c2793c85a86e23f15fdb5cdff1907e83ca675
Max P <Mail@MPassarello.de>
2024-08-18 23:01:44 +0200

    Enhance DIComponent with event reflection and injection
    Refactored DIComponent to replace abstract class with decorated class implementation. This change introduces event reflection for propagating events up the hierarchy and the ability to inject event handlers into child components. Updated event handler logic to use private methods and added extensive tests to verify event propagation, reflection, and error handling. Extended and exported necessary symbols and types.

--- src/libs/DIComponent/DIComponent.ts
+++ src/libs/DIComponent/DIComponent.ts
@@ -1,19 +1,27 @@
 import { Component } from 'obsidian';
+import { ImplementsStatic } from 'src/classes/decorators/ImplementsStatic';
+import { Register } from 'ts-injex';
 import { DIComponentCore } from './DIComponentCore';
-import { IDIComponent } from './interfaces/IDIComponent';
+import { IDIComponent, IDIComponent_ } from './interfaces/IDIComponent';
 import { IComponent } from './interfaces/IDIComponentCore';
 import {
-    EventArgsType,
     EventCallback,
     EventKey,
     EventRegistry,
+    InjectDelegate,
+    SpecificEventCallback,
+    SpecificEventMap,
 } from './types/EventTypes';
 import {
     _childrenComponents,
     _IDIComponent,
+    _isReflected,
     _parentComponent,
     _registeredEvents,
+    broadcastEvent,
+    emitEvent,
     isLoaded,
+    onEvent,
     shouldRemoveOnUnload,
 } from './types/IDIComponentSymbols';
 import {
@@ -27,11 +35,55 @@ import { isIDIComponent } from './types/isIDIComponent';
  * Custom implementation of {@link Component}
  * that uses dependency injection for itself.
  */
-export abstract class DIComponent
-    extends DIComponentCore
-    implements IDIComponent
-{
-    protected [_registeredEvents]: EventRegistry = {};
+@Register('IDIComponent_')
+@ImplementsStatic<IDIComponent_>()
+export class DIComponent extends DIComponentCore implements IDIComponent {
+    #hasRegistryEvent(event: EventKey): boolean {
+        return (
+            this[_registeredEvents][event] != null &&
+            this[_registeredEvents][event]?.length > 0
+        );
+    }
+
+    protected [_registeredEvents]: EventRegistry = {
+        reflect: [
+            (...args) => {
+                if (
+                    this[isLoaded] &&
+                    this[_parentComponent] == null && // Only reflect if there is no parent component.
+                    args.length > 0 &&
+                    typeof args[0] === 'string'
+                ) {
+                    this[broadcastEvent](args[0], args.slice(1));
+
+                    return _isReflected;
+                }
+            },
+        ],
+        inject: [
+            (...args) => {
+                if (
+                    this[isLoaded] &&
+                    args.length === 2 &&
+                    typeof args[0] === 'string' &&
+                    typeof args[1] === 'function' &&
+                    this[_registeredEvents][args[0]]?.length > 0 // Check if the target event exists.
+                ) {
+                    try {
+                        (args[1] as InjectDelegate)(this);
+                    } catch (error) {
+                        // eslint-disable-next-line no-console
+                        console.error(
+                            `Error injecting function with args:`,
+                            args,
+                            `Error:`,
+                            error,
+                        );
+                    }
+                }
+            },
+        ],
+    };
 
     /**
      * @inheritdoc
@@ -54,12 +106,13 @@ export abstract class DIComponent
      * @inheritdoc
      */
     public get [_childrenComponents](): (IDIComponent | IComponent)[] {
-        return (
-            (this[_componentInstance]._children as (
-                | IDIComponent
-                | IComponent
-            )[]) ?? []
-        );
+        if (this[_componentInstance]._children == null)
+            this[_componentInstance]._children = [];
+
+        return this[_componentInstance]._children as (
+            | IDIComponent
+            | IComponent
+        )[];
     }
 
     /**
@@ -99,12 +152,12 @@ export abstract class DIComponent
      * @param args The arguments to pass to the event.
      * @returns Whether the event has been executed.
      */
-    private executeEvens(event: EventKey, ...args: unknown[]): boolean {
+    #executeEvens(event: EventKey, ...args: unknown[]): boolean {
         let hasEventsExecuted = false;
 
         if (this[isLoaded]) {
             const registeredHandlers = this[_registeredEvents][event] as
-                | EventCallback[]
+                | (EventCallback | undefined)[]
                 | undefined;
 
             if (registeredHandlers != null && registeredHandlers?.length > 0) {
@@ -112,7 +165,10 @@ export abstract class DIComponent
                     hasEventsExecuted = true;
 
                     try {
-                        cb(...args);
+                        const returnValue = cb?.(...args);
+
+                        if (returnValue === _isReflected)
+                            hasEventsExecuted = false;
                     } catch (error) {
                         //@todo: Check if we only log the error or throw it.
                         // eslint-disable-next-line no-console
@@ -133,17 +189,25 @@ export abstract class DIComponent
     /**
      * @inheritdoc
      */
-    emitEvent<EventArgs extends EventArgsType = EventArgsType>(
-        event: EventKey,
-        ...args: EventArgs[]
-    ): void {
+    public [emitEvent]<K extends keyof SpecificEventMap>(
+        event: K,
+        ...args: SpecificEventMap[K]
+    ): void;
+    /**
+     * @inheritdoc
+     */
+    public [emitEvent](event: EventKey, ...args: unknown[]): void;
+    /**
+     * @inheritdoc
+     */
+    public [emitEvent](event: EventKey, ...args: unknown[]): void {
         if (this[isLoaded]) {
-            const hasEventsExecuted = this.executeEvens(event, ...args);
+            const hasEventsExecuted = this.#executeEvens(event, ...args);
 
             // Propagate the event to the parent component if it has not been executed.
             if (!hasEventsExecuted) {
                 if (isIDIComponent(this[_parentComponent])) {
-                    this[_parentComponent]?.emitEvent(event, ...args);
+                    this[_parentComponent]?.[emitEvent](event, ...args);
                 }
             }
         }
@@ -152,17 +216,25 @@ export abstract class DIComponent
     /**
      * @inheritdoc
      */
-    broadcastEvent<EventArgs extends EventArgsType = EventArgsType>(
-        event: EventKey,
-        ...args: EventArgs[]
-    ): void {
+    public [broadcastEvent]<K extends keyof SpecificEventMap>(
+        event: K,
+        ...args: SpecificEventMap[K]
+    ): void;
+    /**
+     * @inheritdoc
+     */
+    public [broadcastEvent](event: EventKey, ...args: unknown[]): void;
+    /**
+     * @inheritdoc
+     */
+    public [broadcastEvent](event: EventKey, ...args: unknown[]): void {
         if (this[isLoaded]) {
-            this.executeEvens(event, ...args);
+            this.#executeEvens(event, ...args);
 
             // Broadcast the event to all child components.
             this[_childrenComponents].forEach((child) => {
                 if (isIDIComponent(child)) {
-                    child.emitEvent?.(event, ...args);
+                    child?.[broadcastEvent]?.(event, ...args);
                 }
             });
         }
@@ -171,10 +243,18 @@ export abstract class DIComponent
     /**
      * @inheritdoc
      */
-    on<EventArgs extends EventArgsType = EventArgsType>(
-        event: EventKey,
-        cb: EventCallback<EventArgs>,
-    ): void {
+    [onEvent]<K extends keyof SpecificEventMap>(
+        event: K,
+        cb: SpecificEventCallback<K>,
+    ): void;
+    /**
+     * @inheritdoc
+     */
+    public [onEvent](event: EventKey, cb: EventCallback): void;
+    /**
+     * @inheritdoc
+     */
+    public [onEvent](event: EventKey, cb: EventCallback): void {
         const eventRegister = this[_registeredEvents] as EventRegistry;
 
         if (!eventRegister[event]) {
--- src/libs/DIComponent/types/EventTypes.ts
+++ src/libs/DIComponent/types/EventTypes.ts
@@ -1,6 +1,38 @@
-export type EventArgsType = unknown[];
-export type EventKey = string | '*';
-export type EventCallback<EventArgs extends EventArgsType = EventArgsType> = (
-    ...args: EventArgs
+import { IDIComponent } from '../interfaces/IDIComponent';
+
+/**
+ * Event key type
+ * Can be a string or:
+ * - '*' to listen to all events,
+ * - 'reflect' to send the event up the hierarchy
+ * to the last parent component which reflects the event
+ * as broadcasted event to all child components.
+ * - 'inject' to send a delegate to the receiver components.
+ * See at {@link InjectDelegate}
+ */
+export type EventKey = string | '*' | 'reflect' | 'inject';
+
+/**
+ * Event callback type
+ */
+export type EventCallback = (...args: unknown[]) => unknown;
+
+/**
+ * Event registry type
+ */
+export type EventRegistry = Record<EventKey, (EventCallback | undefined)[]>;
+
+/**
+ * Delegate to inject into the receiver components.
+ */
+export type InjectDelegate = (ctx: IDIComponent & unknown) => void;
+
+export type SpecificEventMap = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'common-window-classes': [string[]];
+    inject: [InjectDelegate];
+};
+
+export type SpecificEventCallback<K extends keyof SpecificEventMap> = (
+    ...args: SpecificEventMap[K]
 ) => void;
-export type EventRegistry = Record<EventKey, EventCallback[]>;
