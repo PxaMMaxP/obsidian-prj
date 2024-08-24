4f5267bf1c11c388bad252bd83a879d341f8e3e7
Max P <Mail@MPassarello.de>
2024-08-24 20:20:46 +0200

    Remove unused types and interface
    - Removed `TransformerDelegate` and `ValidatorDelegate` imports.
    - Changed `InputType` type from 'textArea' to 'textarea'.
    - Deleted the `IInputSettings` interface.

--- src/libs/Settings/SettingColumns/interfaces/IInput.ts
+++ src/libs/Settings/SettingColumns/interfaces/IInput.ts
@@ -4,8 +4,6 @@ import {
     ISettingColumn,
     ISettingColumnFluentApi,
     ISettingColumnProtected,
-    type TransformerDelegate,
-    type ValidatorDelegate,
 } from '../../interfaces/ISettingColumn';
 
 /**
@@ -96,7 +94,7 @@ export interface IInputFluentApi
 /**
  * Input types of {@link IInput}.
  */
-export type InputType = 'textArea' | 'text' | 'password' | 'number' | 'date';
+export type InputType = 'textarea' | 'text' | 'password' | 'number' | 'date';
 
 /**
  * Element types of {@link IInput}.
@@ -109,75 +107,3 @@ export type InputElementType = 'HTMLInputElement' | 'HTMLTextAreaElement';
  * @returns The new value of the input field.
  */
 export type OnChangeCallback = (value: string) => void;
-
-/**
- * Represents an interface for the settings of the input field.
- */
-export interface IInputSettings {
-    /**
-     * Gets the element type of the input field.
-     * @returns The element type of the input field
-     * as either `input` or `textarea`.
-     * @default 'input'
-     */
-    get inputElType(): 'input' | 'textarea';
-
-    /**
-     * The type of the input field.
-     * @default 'text'
-     */
-    inputType: InputType;
-
-    /**
-     * The key of the input field
-     * for the result event.
-     */
-    key: string;
-
-    /**
-     * A transformer that transforms the value of the input field
-     * before it is emitted in the result event.
-     */
-    transformer: TransformerDelegate | undefined;
-
-    /**
-     * Tells whether the input field is required.
-     */
-    isRequired: boolean | ValidatorDelegate;
-
-    /**
-     * The value of the input field.
-     * @default ''
-     */
-    value: string;
-
-    /**
-     * The placeholder of the input field.
-     * @default ''
-     */
-    placeholder: string;
-
-    /**
-     * A callback that is called when the value of the input field changes.
-     * @default undefined
-     */
-    onChangeCallback?: OnChangeCallback;
-
-    /**
-     * A callback that is called to get suggestions for the input field.
-     * @default undefined
-     * @remarks The suggestions are only shown if the callback is set.
-     */
-    getSuggestionsCallback?: GetSuggestionsCallback<string>;
-
-    /**
-     * Whether the toggle is disabled.
-     * @default false
-     */
-    isDisabled: boolean;
-
-    /**
-     * Whether the input field should be spell checked.
-     */
-    shouldSpellCheck: boolean;
-}
