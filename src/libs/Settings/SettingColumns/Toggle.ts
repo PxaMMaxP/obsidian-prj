a6cf04d493846049c9dce733b116908f83b518d9
Max P <Mail@MPassarello.de>
2024-08-26 11:58:03 +0200

    Refactor Toggle class and interfaces
    - **Toggle.ts**
      - Removed dependency on `DIComponent`
      - Refactored `Toggle` class to extend `SettingColumn`
      - Simplified properties initialization and method implementations
      - Changed visibility of `_flowConfig` to `protected`
    - **IToggle.ts**
      - Renamed interface `IToggleFluentAPI` to `IToggleFluentApi`
      - Added `IToggleSettings` and `IToggleElements` interfaces

--- src/libs/Settings/SettingColumns/Toggle.ts
+++ src/libs/Settings/SettingColumns/Toggle.ts
@@ -1,25 +1,20 @@
-import { Component } from 'obsidian';
 import { Implements_ } from 'src/classes/decorators/Implements';
-import type { ILogger_, ILogger } from 'src/interfaces/ILogger';
-import { emitEvent, onEvent } from 'src/libs/DIComponent';
-import { DIComponent } from 'src/libs/DIComponent/DIComponent';
-import { Flow } from 'src/libs/HTMLFlow/Flow';
 import { IFlowApi } from 'src/libs/HTMLFlow/interfaces/IFlow';
 import { EventsParameters } from 'src/libs/HTMLFlow/types/IFlow';
 import { IFlowConfig } from 'src/libs/HTMLFlow/types/IFlowDelegates';
 import { Register } from 'ts-injex';
-import { Inject } from 'ts-injex';
-import { ConfigurationError } from './interfaces/Exceptions';
 import {
-    IToggleFluentAPI,
+    IToggle,
+    IToggleElements,
+    IToggleFluentApi,
     IToggleProtected,
+    IToggleSettings,
     OnChangeCallback,
 } from './interfaces/IToggle';
+import { SettingColumn } from './SettingColumn';
 import type {
     ISettingColumn_,
     SettingColumnConfigurator,
-    TransformerDelegate,
-    ValidatorDelegate,
 } from '../interfaces/ISettingColumn';
 import type { ISettingRowProtected } from '../interfaces/ISettingRow';
 
@@ -28,33 +23,16 @@ import type { ISettingRowProtected } from '../interfaces/ISettingRow';
  */
 @Register('SettingFields.toggle')
 @Implements_<ISettingColumn_<typeof Toggle>>()
-export class Toggle extends DIComponent implements IToggleProtected {
-    @Inject('ILogger_', (x: ILogger_) => x.getLogger(''), false)
-    protected _logger?: ILogger;
-
-    /**
-     * @inheritdoc
-     */
-    public readonly parentSettingItem: ISettingRowProtected & Component;
-
-    /**
-     * @inheritdoc
-     */
-    public readonly elements: {
-        /**
-         * @inheritdoc
-         */
-        toggleEl: HTMLInputElement;
-        /**
-         * The container of the toggle.
-         */
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        _toggleContainerEl: HTMLDivElement;
-    } = {
-        toggleEl: null as never,
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        _toggleContainerEl: null as never,
-    };
+export class Toggle
+    extends SettingColumn<
+        IToggleFluentApi,
+        IToggleElements,
+        IToggleSettings,
+        IToggleProtected
+    >
+    implements IToggle, IToggleProtected, IToggleFluentApi
+{
+    private _isToggled = false;
 
     /**
      * @inheritdoc
@@ -63,7 +41,7 @@ export class Toggle extends DIComponent implements IToggleProtected {
         return this._isToggled;
     }
 
-    private readonly _flowConfig: IFlowConfig<keyof HTMLElementTagNameMap> = (
+    protected readonly _flowConfig: IFlowConfig<keyof HTMLElementTagNameMap> = (
         parent: IFlowApi<keyof HTMLElementTagNameMap>,
     ) => {
         parent.appendChildEl('div', (checkboxContainer) => {
@@ -75,7 +53,7 @@ export class Toggle extends DIComponent implements IToggleProtected {
                         this._isToggled ? 'is-enabled' : 'is-disabled',
                     ],
                     Events: ((): EventsParameters<'div'> => {
-                        if (this._settings.isDisabled === true)
+                        if (this._settings.isDisabled !== true)
                             return [
                                 'click',
                                 (_, __, flow) => {
@@ -90,7 +68,7 @@ export class Toggle extends DIComponent implements IToggleProtected {
                                         this._isToggled,
                                     );
 
-                                    this.emitResultEvent();
+                                    this.emitResult(this._isToggled);
                                 },
                             ];
                         else return [];
@@ -105,14 +83,6 @@ export class Toggle extends DIComponent implements IToggleProtected {
         });
     };
 
-    private _isToggled = false;
-    private readonly _configurator?: SettingColumnConfigurator<IToggleFluentAPI>;
-    private readonly _settings: IToggleSettings = {
-        key: '',
-        transformer: undefined,
-        isRequired: false,
-    };
-
     /**
      * Creates a new toggle field.
      * @param parentSettingItem The setting item that the field belongs to.
@@ -120,57 +90,18 @@ export class Toggle extends DIComponent implements IToggleProtected {
      */
     constructor(
         parentSettingItem: ISettingRowProtected,
-        configurator?: SettingColumnConfigurator<IToggleFluentAPI>,
+        configurator?: SettingColumnConfigurator<IToggleFluentApi>,
     ) {
-        super();
-
-        this.parentSettingItem = parentSettingItem;
-        this._configurator = configurator;
-    }
-
-    /**
-     * Emits the result event
-     * if the key is not empty.
-     */
-    private emitResultEvent(): void {
-        if (this._settings.key !== '') {
-            this[emitEvent](
-                'result',
-                this._settings.key,
-                this._settings.transformer?.(this._isToggled) ??
-                    this._isToggled,
-            );
-        }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public override onload(): void {
-        this._configurator?.(this);
-        this._isToggled = this._settings.isToggled ?? false;
-        const flow = new Flow(this.parentSettingItem.inputEl, this._flowConfig);
-        this.addChild(flow);
-
-        /**
-         * Register on the loaded event to emit the required results
-         * after all components are loaded.
-         */
-        this[onEvent]('loaded', () => {
-            if (this._settings.key !== '') {
-                this[emitEvent](
-                    'required-results',
-                    this._settings.key,
-                    this._settings.isRequired,
-                );
-            }
+        super(parentSettingItem, configurator, {
+            isToggled: false,
+            onChangeCallback: undefined,
         });
     }
 
     /**
      * @inheritdoc
      */
-    setToggled(isToggled: boolean): IToggleFluentAPI {
+    setToggled(isToggled: boolean): IToggleFluentApi {
         this._settings.isToggled = isToggled;
 
         return this;
@@ -179,103 +110,9 @@ export class Toggle extends DIComponent implements IToggleProtected {
     /**
      * @inheritdoc
      */
-    setDisabled(isDisabled: boolean): IToggleFluentAPI {
-        this._settings.isDisabled = isDisabled;
-
-        return this;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    onChange(callback: OnChangeCallback): IToggleFluentAPI {
+    onChange(callback: OnChangeCallback): IToggleFluentApi {
         this._settings.onChangeCallback = callback;
 
         return this;
     }
-
-    /**
-     * @inheritdoc
-     */
-    setResultKey(
-        key: string,
-        transformer?: TransformerDelegate,
-    ): IToggleFluentAPI {
-        this._settings.key = key;
-        this._settings.transformer = transformer ?? this._settings.transformer;
-
-        return this;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    setRequired(test: unknown, required?: unknown): IToggleFluentAPI {
-        const _test: ValidatorDelegate | undefined =
-            typeof test === 'function'
-                ? (test as ValidatorDelegate)
-                : undefined;
-        const _required: boolean = _test != null ? true : (test as boolean);
-
-        this._settings.isRequired = _test != undefined ? _test : _required;
-
-        return this;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    then(callback: (toggle: IToggleProtected) => void): IToggleFluentAPI {
-        try {
-            callback?.(this);
-        } catch (error) {
-            this._logger?.error(
-                'An error occurred while executing the `then` callback.',
-                'The Component will be unloaded.',
-                'Error:',
-                error,
-            );
-            this.unload();
-            throw new ConfigurationError(`then-Callback`, error);
-        }
-
-        return this;
-    }
-}
-
-/**
- * Represents the settings for the toggle field.
- */
-interface IToggleSettings {
-    /**
-     * The value of the toggle.
-     */
-    isToggled?: boolean;
-
-    /**
-     * Whether the toggle is disabled.
-     */
-    isDisabled?: boolean;
-
-    /**
-     * A callback that is called when the value of the toggle changes.
-     */
-    onChangeCallback?: OnChangeCallback;
-
-    /**
-     * The key of the input field
-     * for the result event.
-     */
-    key: string;
-
-    /**
-     * A transformer that transforms the value of the input field
-     * before it is emitted in the result event.
-     */
-    transformer: TransformerDelegate | undefined;
-
-    /**
-     * Tells whether the input field is required.
-     */
-    isRequired: boolean | ValidatorDelegate;
 }
--- src/libs/Settings/SettingColumns/interfaces/IToggle.ts
+++ src/libs/Settings/SettingColumns/interfaces/IToggle.ts
@@ -1,5 +1,9 @@
 import { IDIComponent } from 'src/libs/DIComponent/interfaces/IDIComponent';
 import {
+    ISettingColumnElements,
+    ISettingColumnSettings,
+} from './ISettingColumn';
+import {
     ISettingColumn,
     ISettingColumnFluentApi,
     ISettingColumnProtected,
@@ -8,7 +12,7 @@ import {
 /**
  * Represents a toggle field.
  * @see {@link IToggleProtected}
- * @see {@link IToggleFluentAPI}
+ * @see {@link IToggleFluentApi}
  */
 export interface IToggle extends IDIComponent, ISettingColumn {
     /**
@@ -43,37 +47,60 @@ export interface IToggleProtected extends ISettingColumnProtected {
  * The fluent API for the toggle field.
  * @see {@link IToggle}
  */
-export interface IToggleFluentAPI
-    extends ISettingColumnFluentApi<IToggleFluentAPI> {
+export interface IToggleFluentApi
+    extends ISettingColumnFluentApi<IToggleFluentApi> {
     /**
      * Sets the value of the toggle.
      * @param isToggled The value of the toggle.
      * @returns The toggle fluent API.
      */
-    setToggled(isToggled: boolean): IToggleFluentAPI;
+    setToggled(isToggled: boolean): IToggleFluentApi;
 
     /**
      * Disables or enables the toggle.
      * @param isDisabled Whether the toggle should be disabled.
      * @returns The toggle fluent API.
      */
-    setDisabled(isDisabled: boolean): IToggleFluentAPI;
+    setDisabled(isDisabled: boolean): IToggleFluentApi;
 
     /**
      * Sets a callback that is called when the value of the toggle changes.
      * @param callback The callback that is called when the value of the toggle changes.
      */
-    onChange(callback: OnChangeCallback): IToggleFluentAPI;
-
-    /**
-     * Method for modifying the toggle field.
-     * @param callback The callback that modifies the toggle field.
-     * @returns The toggle fluent API.
-     */
-    then(callback: (toggle: IToggleProtected) => void): IToggleFluentAPI;
+    onChange(callback: OnChangeCallback): IToggleFluentApi;
 }
 
 /**
  * A callback that is called when the value of the toggle changes.
  */
 export type OnChangeCallback = (isToggled: boolean) => void;
+
+/**
+ * Represents the settings for the toggle field.
+ */
+export interface IToggleSettings extends ISettingColumnSettings {
+    /**
+     * The value of the toggle.
+     */
+    isToggled?: boolean;
+
+    /**
+     * A callback that is called when the value of the toggle changes.
+     */
+    onChangeCallback?: OnChangeCallback;
+}
+
+/**
+ * Represents the elements of the toggle field.
+ */
+export interface IToggleElements extends ISettingColumnElements {
+    /**
+     * The toggle element.
+     */
+    toggleEl: HTMLInputElement;
+    /**
+     * The container element of the toggle.
+     */
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    _toggleContainerEl: HTMLDivElement;
+}
